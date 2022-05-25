297aeddf339a0134fb9dfb42d9614150e3849232
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-05-25 10:01:07 +0100

    fixed email sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,38 @@
 import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, provider } from "../firebase";
 import { useNavigate } from "react-router-dom";
-import "../stylesheets/login.css";
-import Button from "./Button";
-import student from "../assets/laptop_lady.svg";
 import { useState } from "react";
 
+import "../stylesheets/login.css";
+
+import Button from "./Button";
+import student from "../assets/laptop_lady.svg";
+
 const Login = () => {
-  const [isValid, setIsValid] = useState(false);
+  const [loginCredentials, setLoginCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    let value = e.target.value;
 
-  // const validatePassword = () => {
-  //   setIsValid(true);
-  //   if (password !== "" && confirmPassword !== "") {
-  //     if (password !== confirmPassword) {
-  //       setIsValid(false);
-  //       setError("Passwords does not match");
-  //     }
-  //   }
-  //   return isValid;
-  // };
+    setLoginCredentials({
+      ...loginCredentials,
+      [e.target.name]: value,
+    });
+  };
 
-  // const SignUpWithEmail = (e) => {
-  //   e.preventDefault();
-  //   setError("");
-  //   if (validatePassword()) {
-  //     // Create a new user with email and password using firebase
-  //     createUserWithEmailAndPassword(auth, email, password)
-  //       .then((res) => {
-  //         console.log(res.user);
-  //       })
-  //       .catch((err) => setError(err.message));
-  //   }
-  //   setEmail("");
-  //   setPassword("");
-  //   setConfirmPassword("");
-  // };
+  const handleSignup = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, loginCredentials.email, loginCredentials.password)
+      .then((res) => {
+        console.log(res);
+        // navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const navigate = useNavigate();
 
@@ -42,9 +40,9 @@ const Login = () => {
     e.preventDefault();
     signInWithPopup(auth, provider)
       .then((res) => {
-        console.log(res);
-        localStorage.removeItem("user");
-        localStorage.setItem("user", JSON.stringify(res.user));
+        // console.log(res);
+        // localStorage.removeItem("user");
+        // localStorage.setItem("user", JSON.stringify(res.user));
         navigate("/admin/dashboard");
       })
       .catch((error) => {
@@ -67,16 +65,33 @@ const Login = () => {
           />
         </div>
         <div className="rt-cont">
-          <form>
+          <form onSubmit={handleSignup}>
             <label>
-              <input type="text" placeholder="Enter your email" />
+              <input
+                type="text"
+                placeholder="Enter your email"
+                value={loginCredentials.email}
+                onChange={handleChange}
+                name="email"
+              />
             </label>
             <label>
-              <input type="password" placeholder="Enter your password" />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                value={loginCredentials.password}
+                onChange={handleChange}
+                name="password"
+              />
             </label>
-            <Button text="Sign In" type="btn btn-rounded-sm btn-sm btn-blue" />
+            <button type="submit">
+              <Button
+                text="Register"
+                type="btn btn-rounded-sm btn-sm btn-blue"
+              />
+            </button>
 
-            <div onClick={(e) => SignInWithGoogleFunc(e)}>
+            <div onClick={SignInWithGoogleFunc}>
               <Button
                 text="Continue with Google"
                 type="btn btn-rounded-sm btn-sm btn-blue"
