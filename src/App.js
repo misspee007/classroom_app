cde8df16bed72e0a678acb3458e1b055c64e8cf9
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-06-07 02:39:54 +0100

    fix mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
           <Route path="*" element={<Navigate to={"/"} replace />} />
         </Routes>
 
-        {isCustomLayout && <Footer />}
+        {isMobile ? <Footer /> : isCustomLayout && <Footer />}
       </DataContext.Provider>
     </div>
   );
